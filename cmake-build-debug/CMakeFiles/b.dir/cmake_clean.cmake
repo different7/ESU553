70914e167d280b1e2e9dfbc35993280b5ebabffc
Hjh <2642753850@qq.com>
2022-01-08 19:07:29 +0800

    TRUE README

---
+++ cmake-build-debug/CMakeFiles/b.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/b.dir/practice/3_11.cpp.obj"
+  "b.exe"
+  "b.exe.manifest"
+  "b.lib"
+  "b.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/b.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/e.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/e.dir/practice/3_16.cpp.obj"
+  "e.exe"
+  "e.exe.manifest"
+  "e.lib"
+  "e.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/e.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
