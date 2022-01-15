65d3ffc2835fc0aa0d8b3c564862d8bfe2a01654
different7 <2642753850@qq.com>
2022-01-15 19:00:32 +0800

    第4章 类与对象

--- practice/4_4.cpp
+++ practice/4_4.cpp
@@ -10,11 +10,11 @@ using namespace std;
 
 class Point{
 public:
-    Point(int xx = 0, int yy = 0){
+    Point(int xx = 0, int yy = 0){  //构造函数
         x = xx;
         y = yy;
     }
-    Point(Point &p);
+    Point(Point &p);  //复制构造函数
     int getX(){return x;}
     int getY(){return y;}
 private:
@@ -26,11 +26,11 @@ Point::Point(Point &p){  //复制构造函数的实现
     y = p.y;
     cout<<"Calling the copy constructor of Point"<<endl;
 }
-
-//类的组合
+//--------------------------------------------------------------------------
+//组合类
 class Line{
 public:
-    Line(Point xp1, Point xp2);
+    Line(Point xp1, Point xp2);  //两个Point类对象作形参的Line类构造函数
     Line(Line &l);
     double getLen() {return len;}
 private:
@@ -38,7 +38,7 @@ private:
     double len;
 };
 
-//组合类的构造函数
+//组合类Line的构造函数
 Line::Line(Point xp1, Point xp2) : p1(xp1), p2(xp2){
     cout<<"Calling constructor of Line"<<endl;
     double x = static_cast<double>(p1.getX()-p2.getX());
@@ -46,16 +46,16 @@ Line::Line(Point xp1, Point xp2) : p1(xp1), p2(xp2){
     len = sqrt(x*x + y*y);
 }
 
-//组合类的复制构造函数
+//组合类Line的复制构造函数
 Line::Line(Line &l):p1(l.p1), p2(l.p2){
     cout<<"Calling the copy constructor of Line"<<endl;
     len = l.len;
 }
-
+//-----------------------------------------------------------------------------
 int main(){
     Point myp1(1,1), myp2(4,5);
-    Line line(myp1,myp2);
-    Line line2(line);
+    Line line(myp1,myp2);  //4次Point复制构造函数，1次line构造函数
+    Line line2(line);      //2次Point复制构造函数，1次line复制构造函数
     cout<<"The length of the line is:";
     cout<<line.getLen()<<endl;
     cout<<"The length of the line2 is:";
