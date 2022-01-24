931563418122e68c33c521f2ad810bc956b85252
different7 <2642753850@qq.com>
2022-01-24 21:20:02 +0800

    习题5-11,自定义头文件

---
+++ Chapter_5/5__11_fn1.h
@@ -0,0 +1,11 @@
+/**
+ * Created by Hjh on 2022-01-24 16:11
+ */#ifndef INC_553CPP_5__11_FN1_H
+#define INC_553CPP_5__11_FN1_H
+
+#endif //INC_553CPP_5__11_FN1_H
+extern int n;
+
+void fn1(){
+    n = 67;
+}
