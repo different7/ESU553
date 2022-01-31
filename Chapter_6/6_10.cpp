d0127afaadaa11abdf90b525cb7cdad6f8efdfdf
different7 <2642753850@qq.com>
2022-02-01 00:26:23 +0800

    例6-10

---
+++ Chapter_6/6_10.cpp
@@ -0,0 +1,23 @@
+/**
+ * Created by Hjh on 2022-01-31 22:46
+ * 读入3个浮点数，分别输出整数部分和小数部分
+ */
+#include <iostream>
+using namespace std;
+
+void splitFloat(float x, int *intPart, float *fracPart){  //指针作为参数
+    *intPart = static_cast<int>(x);  //取x的整数部分
+    *fracPart = x - *intPart;        //取x的小数部分
+}
+
+int main(){
+    cout << "Enter 3 float point numbers:"<<endl;
+    for(int i = 0; i < 3; i++){
+        float x, f;
+        int n;
+        cin>>x;
+        splitFloat(x,&n,&f);  //变量地址作为实参
+        cout<<"Integer Part = "<<n<<", Fraction Part = "<<f<<endl;
+    }
+    return 0;
+}
